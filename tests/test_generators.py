ab20a254d65f38a45086b14ae22e094508ff9b64
Rasan Rasch <rasan@nyu.edu>
2024-05-03 00:42:48 -0400

    Add function to test IfSuppressedFile generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,10 +1,20 @@
 import pytest
 from invenio_access.models import RoleNeed
 from invenio_access.permissions import authenticated_user, superuser_access, any_user
+from invenio_files_rest.models import Bucket, Location, ObjectVersion
+from io import BytesIO
 
 import sys
-sys.path.append('../ultraviolet_permissions')
-from ultraviolet_permissions.generators import AdminSuperUser, Depositor, Viewer, RestrictedDataUser, PublicViewer, Curator
+sys.path.append("../ultraviolet_permissions")
+from ultraviolet_permissions.generators import (
+    AdminSuperUser,
+    Depositor,
+    Viewer,
+    RestrictedDataUser,
+    PublicViewer,
+    Curator,
+    IfSuppressedFile,
+)
 
 
 def test_admin_superuser():
@@ -60,3 +70,25 @@ def test_curator(user_roles_propriatery_record, propriatery_record):
 
     assert generator.needs(record=record) == [RoleNeed("curator")]
     assert generator.needs(record=other_record) == []
+
+
+def test_suppressed(db, bucket_from_dir, create_real_record, location):
+    generator = IfSuppressedFile()
+
+    suppressed_location = Location.get_by_name("suppressed")
+
+    assert suppressed_location
+
+    suppressed_bucket = Bucket.create(suppressed_location)
+    ObjectVersion.create(
+        suppressed_bucket, key="suppressed.txt", stream=BytesIO(b"suppressed")
+    )
+    record = create_real_record(bucket=suppressed_bucket)
+
+    assert generator._condition(record) == True
+
+    bucket = Bucket.create(location)
+    ObjectVersion.create(bucket, key="public.txt", stream=BytesIO(b"public"))
+    record = create_real_record(bucket=bucket)
+
+    assert generator._condition(record) == False
